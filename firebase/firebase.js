b10a31d93d4d88d841fb884a0a502d8784256e1d
Hira <naimulrahman32@gmail.com>
2023-05-14 19:01:13 +0600

    project initialized. pages completed

---
+++ firebase/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAPltEUSwiDfbSeyfb2b4jCWiFP18V2eKA",
+  authDomain: "todo-app-next-js.firebaseapp.com",
+  projectId: "todo-app-next-js",
+  storageBucket: "todo-app-next-js.appspot.com",
+  messagingSenderId: "846202361860",
+  appId: "1:846202361860:web:7e81aa2b8e677df9f43d51",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
